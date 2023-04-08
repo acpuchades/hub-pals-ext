f6689031dc084b397ad1846b8f6048106b007c12
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2023-04-08 17:12:07 +0200

    Arrange numeric outliers by relevance

--- src/qc/numeric-outliers.r
+++ src/qc/numeric-outliers.r
@@ -13,32 +13,39 @@ find_numeric_outliers <- function(data, ..., min_zscore = 3) {
             values_to = "param_value"
         ) |>
         group_by(param_name) |>
-        mutate(param_zscore = c(scale(param_value))) |>
+        mutate(param_zscore = as.vector(scale(param_value))) |>
         ungroup() |>
-        filter(abs(param_zscore) >= min_zscore) |>
-        arrange(desc(abs(param_zscore)))
+        filter(abs(param_zscore) >= min_zscore)
+}
+
+omit_records_from <- function(data, path, by = NULL) {
+    if (file.exists(path)) {
+        ignored <- read_csv(path)
+        by <- ifelse(!is.null(by), by, colnames(ignored))
+        data <- data |> anti_join(ignored, by = by)
+    }
+    data
+}
+
+order_by_relevance <- function(data) {
+    data |>
+        group_by(id_paciente) |>
+        mutate(max_abs_zscore = max(abs(param_zscore))) |>
+        ungroup() |>
+        arrange(desc(max_abs_zscore), id_paciente, desc(abs(param_zscore))) |>
+        select(-max_abs_zscore)
 }
 
 nutrition_outliers <- ufmn_nutrition |>
     find_numeric_outliers(id_paciente, id_visita, fecha_visita) |>
+    omit_records_from("src/qc/nutrition-outliers.ignore") |>
     left_join(ufmn_patients, by = "id_paciente", multiple = "all") |>
-    select(id_paciente, nhc, cip, id_visita, fecha_visita, starts_with("param_"))
-
-nutrition_outliers_ignore_path <- "src/qc/nutrition-outliers.ignore"
-if (file.exists(nutrition_outliers_ignore_path)) {
-    ignored <- read_csv(nutrition_outliers_ignore_path)
-    nutrition_outliers <- nutrition_outliers |>
-        anti_join(ignored, by = c("id_visita", "param_name"))
-}
+    select(id_paciente, nhc, cip, id_visita, fecha_visita, starts_with("param_")) |>
+    order_by_relevance()
 
 respiratory_outliers <- ufmn_respiratory |>
     find_numeric_outliers(id_paciente, id_visita, fecha_visita) |>
+    omit_records_from("src/qc/respiratory-outliers.ignore") |>
     left_join(ufmn_patients, by = "id_paciente", multiple = "all") |>
-    select(id_paciente, nhc, cip, id_visita, fecha_visita, starts_with("param_"))
-
-respiratory_outliers_ignore_path <- "src/qc/respiratory-outliers.ignore"
-if (file.exists(respiratory_outliers_ignore_path)) {
-    ignored <- read_csv(respiratory_outliers_ignore_path)
-    respiratory_outliers <- respiratory_outliers |>
-        anti_join(ignored, by = c("id_visita", "param_name"))
-}
+    select(id_paciente, nhc, cip, id_visita, fecha_visita, starts_with("param_")) |>
+    order_by_relevance()
